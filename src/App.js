04b49c3f7acb4d23bc08bdbd1030495cb423db27
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-04-17 17:07:24 +0600

    router part is setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/AboutUs';
+import Login from './Pages/Login/Login/Login';
+import SignUp from './Pages/Login/SignUp/SignUp';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import CheckOut from './Pages/CheckOut/CheckOut';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>ge</h1>
-      <h3>dd</h3>
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/aboutus' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
